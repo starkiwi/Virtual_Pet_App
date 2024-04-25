77ccb71bb63a323a4443ad22ed16cf8d82e68e9c
starkiwi <165960448+starkiwi@users.noreply.github.com>
2024-04-25 09:45:42 -0500

    Update selectImage.js

--- selectImage.js
+++ selectImage.js
@@ -5,12 +5,12 @@ const petImage = document.getElementById('pet_image');
 function selectPet(pet) {
     console.log(pet);
     if (pet === 'cat1')
-        petImage.src = './pet_images/cat1_image.png';
+        petImage.src = './Virtual_Pet_App/pet_images/cat1_image.png';
     else if (pet === 'cat2')
-        petImage.src = './pet_images/cat2_image.png';
+        petImage.src = './Virtual_Pet_App/pet_images/cat2_image.png';
     else if (pet === 'dog1')
-        petImage.src = './pet_images/dog1_image.png';
+        petImage.src = './Virtual_Pet_App/pet_images/dog1_image.png';
     else if (pet === 'dog2')
-        petImage.src = './pet_images/dog2_image.png';
+        petImage.src = './Virtual_Pet_App/pet_images/dog2_image.png';
 }
 
